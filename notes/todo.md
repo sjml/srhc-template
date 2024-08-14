c8b3109ce353fb4697df8ded26e2194695c1f854
Shane Liesegang <shane@techie.net>
2024-08-14 09:29:04 +0800

    correct og dimensions

--- notes/todo.md
+++ notes/todo.md
@@ -1,5 +1,4 @@
 ## style reconstruction
-- fb social should be 628, not 626
 - switch to a single image library that might be more consistent than the constellation getting used now?
   - https://github.com/libvips/libvips
 - consolidate:
--- src/buildFullDist.sh
+++ src/buildFullDist.sh
@@ -4,7 +4,7 @@ set -e
 
 npm run clean
 npm run css:build
-npm run site:build -- --incremental=./content/full/full_md.njk
+npm run site:mdbuild
 node src/book-binding/buildBooks.js
 mkdir -p _site/downloads
 cp static/downloads/* _site/downloads/
