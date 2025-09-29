b55b335d26be1cfd44e1872105602cd1d0a97e2d
Shane Liesegang <shane@techie.net>
2025-09-29 10:12:08 -0400

    cleanup old dprint stuff

--- src/buildFullDist.sh
+++ src/buildFullDist.sh
@@ -10,8 +10,3 @@ deno run -A src/book-binding/buildBooks.ts
 rm -rf _site/downloads
 cp -R static/downloads _site/downloads
 PRODUCTION_MODE=1 deno task build
-
-cd _site
-dprint fmt --config=../resources/html-output-dprint.json
-cd ..
-
